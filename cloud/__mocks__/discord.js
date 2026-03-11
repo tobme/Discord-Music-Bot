ad814fce83007fb9da1e5d55a5e8dfdbd0ff090c
Tobbe <tobbemellberg@hotmail.se>
2026-03-11 23:02:32 +0100

    fix tests

--- cloud/__mocks__/discord.js
+++ cloud/__mocks__/discord.js
@@ -15,11 +15,22 @@ const ActionRowBuilder = jest.fn().mockImplementation(() => ({
     addComponents: mockReturnThis(),
 }));
 
-const SlashCommandBuilder = jest.fn().mockImplementation(() => ({
-    setName: jest.fn().mockReturnThis(),
-    setDescription: jest.fn().mockReturnThis(),
-    toJSON: jest.fn().mockReturnValue({}),
-}));
+const SlashCommandBuilder = jest.fn().mockImplementation(function() {
+    const builder = {
+        setName: jest.fn().mockReturnValue(undefined),
+        setDescription: jest.fn().mockReturnValue(undefined),
+        addStringOption: jest.fn().mockReturnValue(undefined),
+        toJSON: jest.fn().mockReturnValue({}),
+    };
+    builder.setName.mockReturnValue(builder);
+    builder.setDescription.mockReturnValue(builder);
+    builder.addStringOption.mockImplementation((cb) => {
+        const opt = { setName: jest.fn().mockReturnThis(), setDescription: jest.fn().mockReturnThis(), setRequired: jest.fn().mockReturnThis() };
+        cb(opt);
+        return builder;
+    });
+    return builder;
+});
 
 module.exports = {
     SlashCommandBuilder,
